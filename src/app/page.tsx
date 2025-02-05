2fb386f68a1917b75b125491f8f103b69c42eee5
esnoko <elvisnoko18@gmail.com>
2025-02-05 02:12:07 +0200

    Add ClerkProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Connectify</h1>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
