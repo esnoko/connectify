b994dd4cfc2001c9c0e8237a0ffbe30ea6bd82e2
esnoko <elvisnoko18@gmail.com>
2025-02-10 22:51:30 +0200

    add user action file

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -14,7 +14,7 @@ export async function syncUser() {
             where: {
                 clerkId: userId
             }
-        })
+        });
 
         if (existingUser) {
             return existingUser;
@@ -24,14 +24,31 @@ export async function syncUser() {
             data: {
                 clerkId: userId,
                 name: `${user.firstName || ""} ${user.lastName || ""}`,
-                username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
-                email: user.emailAddresses[0].emailAddress,
+                username: user.username ?? user.emailAddresses[0]?.emailAddress.split("@")[0],
+                email: user.emailAddresses[0]?.emailAddress,
                 image: user.imageUrl,
             }
-        })
+        });
+
+        return dbUser;
     } catch (error) {
         console.log("Error in syncUser", error);
-
     }
+}
 
+export async function getUserByClerkId(clerkId:string){
+    return prisma.user.findUnique({
+        where:{
+            clerkId,
+        },
+        include:{
+            _count:{
+                select:{
+                    followers: true,
+                    following: true,
+                    posts: true,
+                }
+            }
+        }
+    })
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/components/themeProvider";
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +43,7 @@ export default function RootLayout({
               <main className="py-8">
                 <div className="max-w7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                    <div className="hidden lg:block lg:col-span-3"><Sidebar/></div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
