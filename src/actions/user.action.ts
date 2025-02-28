3bcde1b0f87c15c1966363c27263924d1215bb69
esnoko <elvisnoko18@gmail.com>
2025-02-28 15:39:14 +0200

    Update

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -70,7 +70,7 @@ export async function getRandomUsers() {
     const userId = await getDbUserid();
 
     if (!userId) return [];
-
+    
     // get 3 random users exclude ourselves & users that we already follow
     const randomUsers = await prisma.user.findMany({
       where: {
